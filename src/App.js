a02bc4dfa4fd81114ab6024068400087033e584b
amar kumar <amarkumar.sharma.124@gmail.com>
2022-12-18 22:28:39 +0530

    ecommerce page section 1 complete

--- src/App.js
+++ src/App.js
@@ -8,92 +8,93 @@ import './App.css';
 import { useStateContext } from './contexts/ContextProvider';
 
 
-function App() {
+function App () {
 
-    const { activeMenu } = useStateContext()
+	const { activeMenu } = useStateContext()
 
 
-    return (
-        <div>
-            <BrowserRouter>
-                <div className='flex relative dark:bg-main-dark-bg'>
+	return (
+		<div>
+			<BrowserRouter>
+				<div className='flex relative dark:bg-main-dark-bg'>
 
 
 					{/* SETTING BUTTON */}
-                    <div className='fixed right-4 bottom-4 ' style={{ zIndex: '1000' }}>
-                        <TooltipComponent content='Settings' position='Top'>
-                            <button
-                                type='button'
-                                className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
-                                style={{
-                                    background: 'blue',
-                                    borderRadius: '50%'
-                                }}>
-                                <FiSettings />
-                            </button>
-                        </TooltipComponent>
-                    </div>
+					<div className='fixed right-4 bottom-4 ' style={{ zIndex: '1000' }}>
+						<TooltipComponent content='Settings' position='Top'>
+							<button
+								type='button'
+								className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
+								style={{
+									background: 'blue',
+									borderRadius: '50%'
+								}}>
+								<FiSettings />
+							</button>
+						</TooltipComponent>
+					</div>
 
 
 
 					{/* SIDE BAR ---> which will toggle in state of "activeMenu"  */}
-                    {
+					{
 						activeMenu
-                        ?
-                        (<div className='w-64 fixed sidebar dark:bg-secondary-dark-bg bg-white'> <Sidebar /></div>)
-                        :
-                        (<div className='dark:bg-secondary-dark-bg'> <Sidebar /></div>)
-                    }
+							?
+							(<div className='w-64 fixed sidebar dark:bg-secondary-dark-bg bg-white'> <Sidebar /></div>)
+							:
+							(<div className='dark:bg-secondary-dark-bg'> <Sidebar /></div>)
+					}
 
 
 
 					{/* NAVIGATION BAR */}
-                    <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-64' : 'flex-2'}`}>
-                        <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
-                            <Navbar />
-                        </div>
-                    </div>
+					<div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-64' : 'flex-2'}`}>
+						<div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+							<Navbar />
+						</div>
 
 
 
 
-					{/* ROUTES */}
-                    <div>
-                        <Routes>
-                            {/* Dashboard */}
-                            <Route path='/' element={<Ecommerce />} />
-                            <Route path='/ecommerce' element={<Ecommerce />} />
 
-                            {/* Pages */}
-                            <Route path='orders' element={<Orders />} />
-                            <Route path='employeescustomers' element={<Employees />} />
-                            <Route path='customers' element={<Customer />} />
+						{/* ROUTES */}
+						<div>
+							<Routes>
+								{/* Dashboard */}
+								<Route path='/' element={<Ecommerce />} />
+								<Route path='/ecommerce' element={<Ecommerce />} />
 
-                            {/* Apps */}
-                            <Route path='calender' element={<Calendar />} />
-                            <Route path='kanban' element={<Kanban />} />
-                            <Route path='editor' element={<Editor />} />
-                            <Route path='colorPicker' element={<ColorPicker />} />
+								{/* Pages */}
+								<Route path='orders' element={<Orders />} />
+								<Route path='employeescustomers' element={<Employees />} />
+								<Route path='customers' element={<Customer />} />
 
-                            {/* Charts */}
-                            <Route path='line' element={<Line />} />
-                            <Route path='area' element={<Area />} />
-                            <Route path='bar' element={<Area />} />
-                            <Route path='bar' element={<Bar />} />
-                            <Route path='pie' element={<Pie />} />
-                            <Route path='financial' element={<Financial />} />
-                            <Route path='colormaping' element={<ColorMapping />} />
-                            <Route path='pyramid' element={<Pyramid />} />
-                            <Route path='pyramid' element={<Stacked />} />
-                        </Routes>
-                    </div>
+								{/* Apps */}
+								<Route path='calender' element={<Calendar />} />
+								<Route path='kanban' element={<Kanban />} />
+								<Route path='editor' element={<Editor />} />
+								<Route path='colorPicker' element={<ColorPicker />} />
+
+								{/* Charts */}
+								<Route path='line' element={<Line />} />
+								<Route path='area' element={<Area />} />
+								<Route path='bar' element={<Area />} />
+								<Route path='bar' element={<Bar />} />
+								<Route path='pie' element={<Pie />} />
+								<Route path='financial' element={<Financial />} />
+								<Route path='colormaping' element={<ColorMapping />} />
+								<Route path='pyramid' element={<Pyramid />} />
+								<Route path='pyramid' element={<Stacked />} />
+							</Routes>
+						</div>
+					</div>
 
 
 
-                </div>
-            </BrowserRouter>
-        </div>
-    );
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
-const Button = () => {
-  return (
-    <div>
-      button
-    </div>
-  )
+const Button = ({ icon, bgColor, color, bgHoverColor, size, text, borderRadius, width }) => {
+	return (
+		<div>
+			<button
+				type="button"
+				style={{ backgroundColor: bgColor, color, borderRadius }}
+				className={` text-${size} p-3  hover:drop-shadow-xl `}
+			>
+				{text}
+			</button>
+		</div>
+	)
 }
 
 export default Button
