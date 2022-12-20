59a46594057ac354bf572f7cc576faaf23ef9d50
amar kumar <amarkumar.sharma.124@gmail.com>
2022-12-20 23:19:24 +0530

    context update to change themes

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,42 +1,42 @@
+
 import React, { createContext, useContext, useState } from 'react';
 
 const StateContext = createContext();
 
 const initialState = {
-    chat: false,
-    chart: false,
-    userProfile: false,
-    notification: false,
-}
-
+	chat: false,
+	cart: false,
+	userProfile: false,
+	notification: false,
+};
 
 export const ContextProvider = ({ children }) => {
-    const [activeMenu, setActiveMenu] = useState(true); 
+	const [screenSize, setScreenSize] = useState(undefined);
+	const [currentColor, setCurrentColor] = useState('#03C9D7');
+	const [currentMode, setCurrentMode] = useState('Light');
+	const [themeSettings, setThemeSettings] = useState(false);
+	const [activeMenu, setActiveMenu] = useState(true);
 	const [isClicked, setIsClicked] = useState(initialState);
-	const [screenSize, setScreenSize] = useState(undefined)
 
+	const setMode = (e) => {
+		setCurrentMode(e.target.value);
+		localStorage.setItem('themeMode', e.target.value);
+	};
+
+	const setColor = (color) => {
+		setCurrentColor(color);
+		localStorage.setItem('colorMode', color);
+	};
 
 	// handling navigation bar icon clicks
-	const handleClick = (clickedItem)=>{
-		setIsClicked({
-			...initialState, [clickedItem]: true  
-		})
-	}
+	const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
 
-    return (
-		<StateContext.Provider value={{ 
-			activeMenu, setActiveMenu, 
-			isClicked, setIsClicked, 
-			handleClick, 
-			currentColor: "rgb(30, 77, 183)", 
-			screenSize, setScreenSize
-		}} >
-			
-            {children}
+	return (
+		// eslint-disable-next-line react/jsx-no-constructed-context-values
+		<StateContext.Provider value={{ currentColor, currentMode, activeMenu, screenSize, setScreenSize, handleClick, isClicked, initialState, setIsClicked, setActiveMenu, setCurrentColor, setCurrentMode, setMode, setColor, themeSettings, setThemeSettings }}>
+			{children}
+		</StateContext.Provider>
+	);
+};
 
-        </StateContext.Provider>
-    )
-}
-
-
-export const useStateContext = () => useContext(StateContext)
+export const useStateContext = () => useContext(StateContext);
